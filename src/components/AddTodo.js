3bfd6c9d494e1c0f43cd8b563e11e283aed357d9
Priz Almarinez <priz.almarinez@gmail.com>
2019-09-09 13:33:21 +0800

    - added jsonplaceholder api

---
+++ src/components/AddTodo.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class AddTodo extends Component {
+    render() {
+        return (
+            <div>
+                <form className='ui form'>
+
+                </form>
+            </div>
+        );
+    }
+}
+
+export default AddTodo; 
--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,25 @@
 import React, { Component } from 'react';
+// api
+import jsonPlaceholder from '../api/jsonPlaceholder'
 
 class App extends Component {
+    componentDidMount() {
+        console.log('hit');
+        jsonPlaceholder.get('/todos').then(response => {
+            console.log(response);
+        })
+    }
     render() {
         return (
-            <div>
-                App
+            <div className='ui container'>
+                <div className='ui grid'>
+                    <div className='column eight wide'>
+                        adasdasd
+                    </div>
+                    <div className='column eight wide'>
+                        adasdasd
+                    </div>
+                </div>
             </div>
         );
     }
